448c9a63835b6fe6bf333920556e6f9339fe2853
Yoon <Ebird0904@gmail.com>
2025-11-05 01:03:15 +0900

    Initial commit

---
+++ src/main/java/me/gapple/taczRoyale/TaczRoyale.java
@@ -0,0 +1,17 @@
+package me.gapple.taczRoyale;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TaczRoyale extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
